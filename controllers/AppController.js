03f562f6b5772f9988a17db4189b88b7642634a3
mikelexx <michael.muri2020@gmail.com>
2024-09-29 14:42:28 +0300

    refactor: fix semistandard format

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,21 +1,20 @@
 import redisClientInstance from '../utils/redis';
 import dbClient from '../utils/db';
 export default class AppController{
-  static getStatus(req, res){
+  static getStatus(_, res){
     console.log('AppController.getStats called');
     const isDbAlive = dbClient.isAlive();
     const isRedisAlive = redisClientInstance.isAlive();
     res.status(200).json({ "redis": isDbAlive, "db": isRedisAlive });
   }
 
-  static async getStats(req, res){
+  static async getStats(_, res){
     try{
       const usersCount = await dbClient.nbUsers();
       const filesCount = await dbClient.nbFiles();
       res.status(200).json({ "users": usersCount, "files": filesCount });
     }catch(err){
       res.status(500).send();
-    }
+    };
   }
-
 }
